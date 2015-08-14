fc29d0471dd4439653222f4713806efb6052ba8e
glawler <lawlerg@LAPTOP1035.V1.com>
2015-08-14 16:24:43 +0100

    print info to console via logging

--- railway/src/TrainsProblem.java
+++ railway/src/TrainsProblem.java
@@ -2,37 +2,41 @@ import com.config.AppConfig;
 import com.exception.PathNotFoundException;
 import com.model.Station;
 import com.service.RailwayService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TrainsProblem {
+
+    static final Logger console = LoggerFactory.getLogger(TrainsProblem.class);
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 
         RailwayService railwayService = ctx.getBean(RailwayService.class);
 
-        System.out.println("Questions 1-5 ----------------------\n");
-        System.out.println("1: " + railwayService.getRouteLength(Station.A, Station.B, Station.C));
-        System.out.println("2: " + railwayService.getRouteLength(Station.A, Station.D));
-        System.out.println("3: " + railwayService.getRouteLength(Station.A, Station.D, Station.C));
-        System.out.println("4: " + railwayService.getRouteLength(Station.A, Station.E, Station.B, Station.C, Station.D));
+        console.warn("Questions 1-5 ----------------------\n");
+        console.info("1: " + railwayService.getRouteLength(Station.A, Station.B, Station.C));
+        console.info("2: " + railwayService.getRouteLength(Station.A, Station.D));
+        console.info("3: " + railwayService.getRouteLength(Station.A, Station.D, Station.C));
+        console.info("4: " + railwayService.getRouteLength(Station.A, Station.E, Station.B, Station.C, Station.D));
         try {
-            System.out.println("5: " + railwayService.getRouteLength(Station.A, Station.E, Station.D));
+            console.info("5: " + railwayService.getRouteLength(Station.A, Station.E, Station.D));
         } catch (PathNotFoundException e) {
-            System.out.println("NO SUCH ROUTE");
+            console.info("NO SUCH ROUTE");
         }
 
-        System.out.println("\nQuestions 6-7 -------------------------\n");
-        System.out.println("6: " + railwayService.getNumberOfRoutesWithStops(Station.C, Station.C, 3));
-        System.out.println("7: " + railwayService.getNumberOfRoutesWithExactNumberOfStops(Station.A, Station.C, 4));
+        console.info("\nQuestions 6-7 -------------------------\n");
+        console.info("6: " + railwayService.getNumberOfRoutesWithStops(Station.C, Station.C, 3));
+        console.info("7: " + railwayService.getNumberOfRoutesWithExactNumberOfStops(Station.A, Station.C, 4));
 
-        System.out.println("\nQuestions 8-9 ----------------------\n");
-        System.out.println("8: " + railwayService.getShortestNonLoopingRouteLength(Station.A, Station.C));
-        System.out.println("9: " + railwayService.getShortestLoopLengthIncludingGivenStation(Station.B));
+        console.info("\nQuestions 8-9 ----------------------\n");
+        console.info("8: " + railwayService.getShortestNonLoopingRouteLength(Station.A, Station.C));
+        console.info("9: " + railwayService.getShortestLoopLengthIncludingGivenStation(Station.B));
 
-        System.out.println("\nQuestion 10 ------------------------\n");
-        System.out.println("10: " + railwayService.getNumberOfRoutesIncludingStationWithinRadius(Station.C, 30));
+        console.info("\nQuestion 10 ------------------------\n");
+        console.info("10: " + railwayService.getNumberOfRoutesIncludingStationWithinRadius(Station.C, 30));
 
-        System.out.println("-------------------------");
+        console.info("-------------------------");
     }
 }
